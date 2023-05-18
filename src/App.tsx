badd6f519c3ee9ffabf3ebf1c0268beedb094c03
jigyasudhingra <68184200+jigyasudhingra@users.noreply.github.com>
2023-05-18 20:53:49 +0530

    Get user data from database on fresh load

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,46 @@ import { Box } from "@mui/material";
 import NavigationMenu from "./Components/NavigationMenu";
 import RouteLayout from "./RootLayout";
 import { UserContext, userDetails } from "./Contexts/UserContext";
-import { useState } from "react";
-import { a } from "./firebase-config";
+import { useEffect, useState } from "react";
+import { db } from "./firebase-config";
+import { doc, getDoc } from "firebase/firestore";
 
-function App() {
+const App = () => {
   const [userInfo, setUserInfo] = useState(userDetails);
+  const [loading, setLoading] = useState(true);
+
   const updateData = (newData: any) => {
     setUserInfo(newData);
   };
 
+  // --- Get user data from database ---
+  const intialization = async () => {
+    const docSnap = await getDoc(doc(db, "users", "jigyasudhingra@gmail.com"));
+
+    if (docSnap.exists()) {
+      updateData(docSnap.data());
+    } else {
+      console.log("No such document!");
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    intialization();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(loading);
   return (
     <UserContext.Provider value={{ userInfo, updateData }}>
       <div style={{ width: "100%" }}>
         <Box width="100%">
           <NavigationMenu />
         </Box>
-        <RouteLayout />
+        {loading ? <div>...loading</div> : <RouteLayout />}
       </div>
     </UserContext.Provider>
   );
-}
+};
 
 export default App;
